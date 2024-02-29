4d51fec92f1cc76bdc026bf1b111021c12eb91ef
Shira Klugmann <shirarklugmann@gmail.com>
2024-02-28 21:23:28 -0500

    Add project files.

---
+++ GitHUb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHUb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
